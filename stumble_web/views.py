d93f2c209f5e2fd0441621b0731dd11e61a4c02e
Zachary DiPasquale <zachary.dipasquale@gmail.com>
2017-07-19 22:57:12 -0500

    added user registration

--- stumble_web/views.py
+++ stumble_web/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import TemplateView
-
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
@@ -11,4 +14,19 @@ class RenderMap(TemplateView):
 
 class CreateAccount(TemplateView):
     template_name = 'stumble/accounts/create-account.html'
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('index')
+    else:
+        form = UserCreationForm()
+    return render(request, 'stumble/accounts/create-account.html', {'form': form})
 
