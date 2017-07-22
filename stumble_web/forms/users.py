1d35fbea25b5cf7ae9d5a16110a092826258419d
Zachary DiPasquale <zachary.dipasquale@gmail.com>
2017-07-21 22:04:18 -0500

    commits

--- stumble_web/forms/users.py
+++ stumble_web/forms/users.py
@@ -1,33 +1,49 @@
 from django import forms
 from ..models import User
 from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
 
 
 class RegistrationUserForm(forms.Form):
     username = forms.RegexField(
         regex=r'^\w+$',
         widget=forms.TextInput(attrs=dict(required=True, max_length=30)),
-        label=_("Username"),
-        error_messages={'invalid': _("This value must contain only letters, numbers and underscores.")},
+        label="Username",
+        error_messages={'invalid': "This value must contain only letters, numbers and underscores."},
+
+    )
+
+    first_name = forms.RegexField(
+        regex=r'^\w+$',
+        widget=forms.TextInput(attrs=dict(required=True, max_length=40)),
+        label="First Name",
+        error_messages={'invalid': "This value must contain only letters, numbers and underscores."},
+
+    )
+    last_name = forms.RegexField(
+        regex=r'^\w+$',
+        widget=forms.TextInput(attrs=dict(required=True, max_length=40)),
+        label="Last Name",
+        error_messages={'invalid': "This value must contain only letters, numbers and underscores."},
 
     )
 
     email = forms.EmailField(
         widget=forms.TextInput(attrs=dict(required=True, max_length=30)),
-        label=_("Email address")
+        label="Email address"
     )
 
     password1 = forms.PasswordInput(attrs=dict(required=True, min_length=6, render_value=False))
 
     password2 = forms.PasswordInput(attrs=dict(required=True, min_length=6, render_value=False,
-                                               label=_("Password (again)")))
+                                               label="Password (again)"))
 
     def clean_username(self):
         try:
             user = User.objects.get(username__iexact=self.cleaned_data['username'])
         except User.DoesNotExist:
             return self.cleaned_data['username']
-        raise forms.ValidationError(_("The username already exists. Please try another one."))
+        raise forms.ValidationError("The username already exists. Please try another one.")
 
     def clean(self):
         if 'password1' in self.cleaned_data and 'password2' in self.cleaned_data:
@@ -36,5 +52,11 @@ class RegistrationUserForm(forms.Form):
         return self.cleaned_data
 
     def __init__(self, *args, **kwargs):
-        super(ExampleForm, self).__init__(*args, **kwargs)
+        super(RegistrationUserForm, self).__init__(*args, **kwargs)
         self.helper = FormHelper()
+        self.helper.form_id = 'creation_form'
+        self.helper.form_class = 'form'
+        self.helper.form_method = 'post'
+        self.helper.form_action = 'create_account'
+
+        self.helper.add_input(Submit('submit', 'Submit'))
--- stumble_web/urls.py
+++ stumble_web/urls.py
@@ -3,11 +3,12 @@ from django.conf.urls import url
 from . import views
 from django.views.generic.base import TemplateView
 from django.contrib.auth import views as auth_views
+from stumble_web.views import CreateAccount
 
 
 urlpatterns = [
     url(r'^$', auth_views.login, {'template_name': 'index.html'}, name='index'),
     url(r'^home', views.RenderMap.as_view(), name='home'),
-    url(r'^createaccount', views.signup, name='createaccount'),
+    url(r'^createaccount', views.CreateAccount.as_view(), name='createaccount'),
     url(r'^logout/$', auth_views.logout, name='logout'),
 ]
